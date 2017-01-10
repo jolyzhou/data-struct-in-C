776cdcebfecc34c4e054005ebb2e5a83b80d1c09
jolyzhou <782228284@qq.com>
2017-01-10 13:34:25 +0800

    add stack

---
+++ stack.c
@@ -0,0 +1,18 @@
+//
+// Created by jolyzhou on 2017/1/10.
+//
+
+#include <stdlib.h>
+
+#include "list.h"
+#include "stack.h"
+
+int stack_push(Stack *stack, const void *data)
+{
+    return list_ins_next(stack, NULL, data);
+}
+
+int stack_pop(Stack *stack, void **data)
+{
+    return list_rem_next(stack, NULL, data);
+}
---
+++ stack.h
@@ -0,0 +1,23 @@
+//
+// Created by jolyzhou on 2017/1/10.
+//
+
+#ifndef DATA_STRUCT_STACK_H
+#define DATA_STRUCT_STACK_H
+
+#include <stdlib.h>
+#include "list.h"
+
+typedef List Stack;
+
+// public interface
+#define stack_init list_init
+#define stack_destory list_destory
+#define stack_peek(stack) ((stack)->head == NULL ? NULL : (stack)->head->data)
+#define stack_size list_size
+
+int stack_push(Stack *stack, const void *data);
+int stack_pop(Stack *stack, void **data);
+
+
+#endif //DATA_STRUCT_STACK_H
