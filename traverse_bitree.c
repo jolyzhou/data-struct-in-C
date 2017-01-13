23af2c770c94d2ea5e1a52c222acd701f44c8d14
jolyzhou <782228284@qq.com>
2017-01-13 23:25:18 +0800

    add traverse tree

---
+++ traverse_bitree.c
@@ -0,0 +1,53 @@
+//
+// Created by jolyzhou on 2017/1/13.
+//
+#include "bitree.h"
+#include "list.h"
+
+// preorder
+int preorder(const BiTreeNode *node, List *list)
+{
+    if (!bitree_is_eob(node)) {
+        if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0)
+            return -1;
+        if (!bitree_is_eob(bitree_left(node)))
+            if (preorder(bitree_left(node), list) != 0)
+                return -1;
+        if (!bitree_is_eob(bitree_right(m=node)))
+            if (preorder(bitree_right(node), list) != 0)
+                return -1;
+    }
+    return 0;
+}
+
+// inorder
+int inorder(const BiTreeNode *node, List *list)
+{
+    if (!bitree_is_eob(node)) {
+        if (!bitree_is_eob(bitree_left(node)))
+            if(inorder(bitree_left(node), list) != 0)
+                return -1;
+        if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0)
+            return -1;
+        if (!bitree_is_eob(bitree_right(node)))
+            if (inorder(bitree_right(node), list) != 0)
+                return -1;
+    }
+    return 0;
+}
+
+// postorder
+int postorder(const BiTreeNode *node, List *list)
+{
+    if (!bitree_is_eob(node)) {
+        if (!bitree_is_eob(bitree_left(node)))
+            if(postorder(bitree_left(node), list) != 0)
+                return -1;
+        if (!bitree_is_eob(bitree_right(node)))
+            if (postorder(bitree_right(node), list) != 0)
+                return -1;
+        if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0)
+            return -1;
+    }
+    return 0;
+}
