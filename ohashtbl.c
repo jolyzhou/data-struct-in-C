49eb09b840dcdc1f3ef572f9940040d4c655525f
jolyzhou <782228284@qq.com>
2017-01-13 09:45:53 +0800

    fix open addr hash table bugs

--- ohashtbl.c
+++ ohashtbl.c
@@ -12,7 +12,7 @@ static char vacated;
 int ohtbl_init(OHTbl *htbl,
                int positions,
                int (*h1)(const void *key),
-               int (*h2)(const void key),
+               int (*h2)(const void *key),
                int (*match)(const void *key1, const void *key2),
                void (*destroy)(void *data))
 {
--- ohashtbl.h
+++ ohashtbl.h
@@ -25,7 +25,7 @@ typedef struct OHTbl_
 int ohtbl_init(OHTbl *htbl,
                int positions,
                int (*h1)(const void *key),
-               int (*h2)(const void key),
+               int (*h2)(const void *key),
                int (*match)(const void *key1, const void *key2),
                void (*destroy)(void *data));
 void ohtbl_destroy(OHTbl *htbl);
