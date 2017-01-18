cbd943319c33c6776334a4768769179f6f8f39c8
jolyzhou <782228284@qq.com>
2017-01-18 14:26:14 +0800

    add merge sort

--- sort.c
+++ sort.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+// ---------------INSERT SORT start-----------------
 // insert sort
 int ins_sort(void *data, int size, int esize, int (*compare)(const void *key1, const void *key2))
 {
@@ -25,7 +27,9 @@ int ins_sort(void *data, int size, int esize, int (*compare)(const void *key1, c
     free(key);
     return 0;
 }
+// ---------------INSERT SORT end-----------------
 
+// ---------------QUICK SORT start-----------------
 // quick sort
 static int compare_int(const void *int1, const void *int2)
 {
@@ -88,6 +92,74 @@ int qksort(void *data,
         if (qksort(data, size, esize, i, j, compare) < 0)
             return -1;
         i = j + 1;
+    }
+    return 0;
+}
+// ---------------QUICK SORT end-----------------
+
+// ---------------MERGE SORT start-----------------
+// merge
+static int merge(void *data, int esize, int i, int j, int k,
+                 int (*compare)(const void *key1, const void *key2))
+{
+    char *a = data, *m;
+    int ipos, jpos, mpos;
+    // initialize the counters used in merging
+    ipos = i;
+    jpos = j + 1;
+    mpos = 0;
+    if ((m = (char *)malloc(esize*((k - i) + 1))) == NULL)
+        return -1;
+    while (ipos <= j || jpos <= k) {
+        if (ipos > j) {
+            // the left division has no more elements to merge
+            while (jpos <= k) {
+                memcpy(&m[mpos*esize], &a[jpos*esize], esize);
+                jpos++;
+                mpos++;
+            }
+            continue;
+        } else if (jpos > k) {
+            // the right division has no more elements to merge
+            while (ipos <= j) {
+                memcpy(&m[mpos*esize], &a[ipos*esize], esize);
+                ipos++;
+                mpos++;
+            }
+            continue;
+        }
+        if (compare(&a[ipos*esize], &a[jpos*esize]) < 0) {
+            memcpy(&m[mpos*esize], &a[ipos*esize], esize);
+            ipos++;
+            mpos++;
+        } else {
+            memcpy(&m[mpos*esize], &a[jpos*esize], esize);
+            jpos++;
+            mpos++;
+        }
+    }
+    memcpy(&a[i*esize], m, esize*((k-i)+1));
+    free(m);
+    return 0;
+}
+
+// mgsort
+int mgsort(void *data, int size, int esize, int i, int k,
+           int (*compare)(const void *key1, const void *key2))
+{
+    int j;
+    // stop the recursion when no more divisions can be made
+    if (i < k) {
+        // determine where to divide the elements
+        j = (int)(((i + k - 1)) / 2);
+        // recursively sort the two divisions
+        if (mgsort(data, size, esize, i, j, compare) < 0)
+            return -1;
+        if (mgsort(data, size, esize, j+1, k, compare) < 0)
+            return -1;
+        // merge the two sorted divisions into a single sorted set
+        if (merge(data, esize, i, j, k, compare) < 0)
+            return -1;
     }
     return 0;
 }
--- sort.h
+++ sort.h
@@ -9,9 +9,10 @@
 int ins_sort(void *data, int size, int esize, int (*compare)(const void *key1, const void *key2));
 // quick sort
 int qksort(void *data,
-           int size,
-           int esize,
-           int i,
-           int k,
+           int size, int esize, int i, int k,
+           int (*compare)(const void *key1, const void *key2));
+// merge sort
+int mgsort(void *data,
+           int size, int esize, int i, int k,
            int (*compare)(const void *key1, const void *key2));
 #endif //DATA_STRUCT_SORT_H
