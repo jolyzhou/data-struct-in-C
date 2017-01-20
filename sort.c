32bdfb4a67b9181b09c616f5f22ef301903eba59
jolyzhou <782228284@qq.com>
2017-01-20 10:17:12 +0800

    add bisearch

--- sort.c
+++ sort.c
@@ -267,3 +267,29 @@ int rxsort(int *data, int size)
     return 0;
 }
 // ---------------RADIX SORT end-----------------
+
+// bisearch
+int bisearch(void *sorted,
+             const void *target,
+             int size, int esize,
+             int (*compare)(const void *key1, const void *key2))
+{
+    int left, middle, right;
+    // continue searching until the left and right indices cross
+    left = 0;
+    right = size - 1;
+    while (left <= right) {
+        middle = (left + right) / 2;
+        switch (compare(((char *)sorted + (esize * middle)), target)) {
+            case -1:
+                left = middle + 1;
+                break;
+            case 1:
+                right = middle - 1;
+                break;
+            case 0:
+                return middle;
+        }
+    }
+    return -1;
+}
